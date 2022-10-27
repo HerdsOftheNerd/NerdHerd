010b19e5531fcd3b4a1fdc50984b4fd1eda08e44
deveshkedia <dkediaphone@icloud.com>
2022-10-27 11:53:51 +0530

    Added city and state models

--- core/notes/models.py
+++ core/notes/models.py
@@ -2,6 +2,33 @@ from django.db import models
 import uuid
 from users.models import Profile
 
+class State(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  name = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  cities = models.ManyToManyField('City', related_name='states', blank=True)
+
+  def __str__(self):
+    return self.name
+
+class City:
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  name = models.CharField(max_length=255)
+  created_at = models.DateTimeField(auto_now_add=True)
+  institutions = models.ManyToManyField('Institution', related_name='cities', blank=True)
+  
+  def __str__(self):
+    return self.name
+  
+class Institution(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    notes = models.ManyToManyField('Note', related_name='institutions', blank=True)
+
+    def __str__(self):
+        return self.name
+
 class Note(models.Model):
   id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
   author = models.ForeignKey(Profile,on_delete=models.CASCADE)
@@ -14,6 +41,10 @@ class Note(models.Model):
   cost = models.FloatField(null=False)
   note_pdf = models.FileField(upload_to='notes',null=False,blank=False)
   preview_page_no = models.IntegerField(default=1)
+  city = models.CharField(max_length=100)
+  state = models.CharField(max_length=100)
+  board = models.CharField(max_length=100)
+  institution = models.CharField(max_length=100)
   
   def __str__(self):
     return f"{self.topic[:50]}..."
@@ -24,6 +55,7 @@ class Review(models.Model):
   note = models.ForeignKey(Note,on_delete=models.CASCADE)
   rating = models.IntegerField(default=0)
   review = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
   
   def __str__(self):
     return f"{self.review[:50]}..."
@@ -33,13 +65,17 @@ class Comment(models.Model):
   author  = models.ForeignKey(Profile,on_delete=models.CASCADE)
   note = models.ForeignKey(Note, on_delete=models.CASCADE)
   comment = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
   
   def __str__(self):
     return f"{self.comment[:50]}..."
 
 class Transaction(models.Model):
+  id = models.UUIDField(default=uuid.uuid4,unique=True,primary_key=True,editable=False)
   seller = models.ForeignKey(Profile,on_delete=models.DO_NOTHING,related_name='seller')
   buyer = models.ForeignKey(Profile,on_delete=models.DO_NOTHING,related_name='buyer')
   note = models.ForeignKey(Note,on_delete=models.DO_NOTHING)
   timestamp = models.DateTimeField(auto_now_add=True)
+  amount = models.FloatField()
   success = models.BooleanField(default=False)
+  # transaction_id = models.CharField(max_length=100)]
